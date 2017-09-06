600a31e7beaf484a3d23b26c708bf32800520b8a
johanhuu <johanhuu@gmail.com>
2017-09-06 13:31:00 +0200

    Create Calculator.java

---
+++ src/Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+	
+	private Calculator() {
+		
+	}
+	
+	public static int sum(int a, int b) {
+		return a+b;
+	}
+	
+	public static int difference(int a, int b) {
+		return a-b;
+	}
+}
