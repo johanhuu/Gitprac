e7d79511d8315820d8bcae91b62b8b02da13f5ca
johanhuu <johanhuu@gmail.com>
2017-09-06 13:50:31 +0200

    Update Something.java

--- src/Something.java
+++ src/Something.java
@@ -4,10 +4,6 @@ public class Something {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("version 1.0");
-		
-		System.out.println(Calculator.difference(10, 5));
-		System.out.println(Calculator.multiply(10, 5));
-		System.out.println(Calculator.square(2, 5));
 	}
 
 }
