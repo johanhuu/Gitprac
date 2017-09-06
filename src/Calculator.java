da1455b8457107e4b7e012951c3e47798212a5d4
Johan Huusmann <johanhuu@gmail.com>
2017-09-06 13:34:45 +0200

    added functionality and testing

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,8 +4,12 @@ public class Calculator {
 		
 	}
 	
-	public static int sum(int a, int b) {
-		return a+b;
+	public static int square(int a, int b) {
+		return (int) Math.pow(a, b);
+	}
+	
+	public static int multiply(int a, int b) {
+		return a*b;
 	}
 	
 	public static int difference(int a, int b) {
