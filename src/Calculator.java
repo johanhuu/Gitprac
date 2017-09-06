831fc4324916cdeb74a8315f13323c3c82b52cd1
johanhuu <johanhuu@gmail.com>
2017-09-06 15:10:14 +0200

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -18,4 +18,8 @@ public class Calculator {
 	public static int add(int a, int b, int c) {
 		return a+b+c;
 	}
+	
+	public static int concat(int a, int b) {
+		return Integer.parseInt(Integer.toString(a) + Integer.toString(b));
+	}
 }
